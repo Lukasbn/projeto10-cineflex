569b3486209b25bc980ca9a83d6b00dc681ac1e5
LucasBento <lubentoneves@gmail.com>
2023-03-12 18:28:02 -0300

    correção data-test-footer da SessionPage

--- src/pages/SessionsPage/SessionsPage.js
+++ src/pages/SessionsPage/SessionsPage.js
@@ -47,7 +47,7 @@ export default function SessionsPage() {
 
             </div>
 
-            <FooterContainer>
+            <FooterContainer data-test="footer">
                 <div>
                     <img src={filme.posterURL} alt={`poster ${filme.title}`} />
                 </div>
